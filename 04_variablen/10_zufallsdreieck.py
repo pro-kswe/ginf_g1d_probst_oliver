a98466743b9709a7391cdf39af001f86c31a6c80
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2025-11-11 09:09:49 +0100

    Zufallsdreieck

---
+++ 04_variablen/10_zufallsdreieck.py
@@ -0,0 +1,27 @@
+import turtle
+import random
+
+x_1 = random.randrange(-100, 101)
+x_2 = random.randrange(-100, 101)
+x_3 = random.randrange(-100, 101)
+y_1 = random.randrange(-200, 276)
+y_2 = random.randrange(-200, 276)
+y_3 = random.randrange(-200, 276)
+d = random.randrange(10, 26)
+farbe_1 = "red"  # Strings sind in Python Text mit Anführungszeichen.
+farbe_2 = "green"
+farbe_3 = "blue"
+stiftstarke = 3  # Integers oder kurz Ints sind in Python ganze Zahlen.
+turtle.pensize(stiftstarke)
+turtle.pu()
+turtle.goto(x_1, y_1)
+turtle.pd()
+turtle.pencolor(farbe_1)
+turtle.dot(d)
+turtle.goto(x_2, y_2)
+turtle.pencolor(farbe_2)
+turtle.dot(d)
+turtle.goto(x_3, y_3)
+turtle.pencolor(farbe_3)
+turtle.dot(d)
+turtle.goto(x_1, y_1)
