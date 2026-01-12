eb2ef558f42bfa2f5c2a6aaeeba20e7b0feb8493
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-01-12 22:26:38 +0100

    Template Essen

---
+++ 02_konsole/02_formatierte_strings/06_essen.py
@@ -0,0 +1,11 @@
+import random as rd
+
+speisen = ["Pommes", "Linsen-Dal", "Spaghetti"]
+speise = rd.choice(speisen)
+# wochentage = 
+# wochentag = 
+print(f"Am gibt es {speise} in der Mensa.")
+print("Wie finden wir das?")
+anzahl = 3
+for _ in range(anzahl):
+    print("Juhu")
