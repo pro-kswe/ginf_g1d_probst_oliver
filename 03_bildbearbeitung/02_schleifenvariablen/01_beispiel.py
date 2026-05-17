71bfc3c5d248ee75b248fb08950aac03aa00823f
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-05-17 17:52:47 +0200

    Leerzeile hinzugefügt

--- 03_bildbearbeitung/02_schleifenvariablen/01_beispiel.py
+++ 03_bildbearbeitung/02_schleifenvariablen/01_beispiel.py
@@ -4,4 +4,4 @@ bild = img.new("RGB", (5, 4))
 farbe = (255, 255, 255)
 for x in range(0, 5):
     bild.putpixel((x, 1), farbe)
-bild.save("01_beispiel_ergebnis.png") 
+bild.save("01_beispiel_ergebnis.png")
