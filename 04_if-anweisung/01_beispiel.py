2020f1f4f8668a0a4ecb09a2d85334f45f0fa7c3
Oliver Probst <65253973+pro-kswe@users.noreply.github.com>
2026-06-01 08:49:31 +0200

    Beispiel

---
+++ 04_if-anweisung/01_beispiel.py
@@ -0,0 +1,9 @@
+vorname = input("Wie heisst du?")
+print(f"Hallo {vorname}. Schön dich zu sehen.")
+
+if vorname == "Alice":
+    print(f"{vorname} ist ein schöner Name.")
+
+print("Tschüss")
+
+# Zum Programmieren: konsole.ginf.ch verwenden
